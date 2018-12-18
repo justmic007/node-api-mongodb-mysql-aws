8ef880fd2bae517ef1f031e6602b6fe18e8f2ed2
justmic007 <justmic007@gmail.com>
2018-12-18 17:09:10 +0100

    addeded automatic reloading- nodemon, default logging and error handling

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+// Adding a loggin package
+const morgan = require('morgan');
+
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
 
@@ -12,7 +15,27 @@ const ordersRoutes = require('./api/routes/orders');
 // });
 
 // This is a filter essentially
+
+app.use(morgan('dev'));
+
+// Middleware
 app.use('/products', productRoutes);
 app.use('/orders', ordersRoutes);
+
+// Handles errors that bypass the above Middleware
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+})
 
 module.exports = app;
